323f5309b9fca11f9f61c90d787ec24501777e3a
armanpinachyan <pinachyan99@mail.ru>
2023-07-20 01:07:43 +0400

    01:07

--- js/script.js
+++ js/script.js
@@ -200,3 +200,45 @@ function myFunction() {
         title_mobile.classList.remove('text')
     }
 }
+
+
+//........... Slider
+
+
+$('.slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
