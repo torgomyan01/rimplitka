b97a90b284b77273e23d77ca60d5a9d8c2f558e1
armanpinachyan <pinachyan99@mail.ru>
2023-07-20 14:30:09 +0400

    14:30

--- js/script.js
+++ js/script.js
@@ -210,13 +210,13 @@ $('.slider').slick({
     infinite: false,
     speed: 300,
     slidesToShow: 5,
-    slidesToScroll: 2,
+    slidesToScroll: 5,
     responsive: [
         {
             breakpoint: 1024,
             settings: {
                 slidesToShow: 3,
-                slidesToScroll: 1,
+                slidesToScroll: 3,
                 infinite: true,
                 dots: true
             }
@@ -225,7 +225,7 @@ $('.slider').slick({
             breakpoint: 600,
             settings: {
                 slidesToShow: 2,
-                slidesToScroll: 1,
+                slidesToScroll: 3,
                 dots: false
             }
         },
@@ -242,3 +242,34 @@ $('.slider').slick({
         // instead of a settings object
     ]
 });
+
+let statusSlider = true;
+
+$(window).on('resize', function (){
+    if($(window).width() <= 768 && statusSlider){
+        statusSlider = false;
+        console.log('sddddd')
+        // $('.slider').slick({
+        //     dots: true,
+        //     infinite: false,
+        //     speed: 300,
+        //     slidesToShow: 5,
+        //     slidesToScroll: 2,
+        //     responsive: [
+        //         {
+        //             breakpoint: 1024,
+        //             settings: {
+        //                 slidesToShow: 3,
+        //                 slidesToScroll: 1,
+        //                 infinite: true,
+        //                 dots: true
+        //             }
+        //         }
+        //         // You can unslick at a given breakpoint now by adding:
+        //         // settings: "unslick"
+        //         // instead of a settings object
+        //     ]
+        // });
+
+    }
+}).resize()
