d4f9ceba464258b949c62393a3962749172de970
armanpinachyan <pinachyan99@mail.ru>
2023-07-29 01:31:04 +0400

    01:31

--- js/script.js
+++ js/script.js
@@ -260,27 +260,27 @@ $(window).on('resize', function (){
     if($(window).width() <= 768 && statusSlider){
         statusSlider = false;
         console.log('sddddd')
-        // $('.slider').slick({
-        //     dots: true,
-        //     infinite: false,
-        //     speed: 300,
-        //     slidesToShow: 5,
-        //     slidesToScroll: 2,
-        //     responsive: [
-        //         {
-        //             breakpoint: 1024,
-        //             settings: {
-        //                 slidesToShow: 3,
-        //                 slidesToScroll: 1,
-        //                 infinite: true,
-        //                 dots: true
-        //             }
-        //         }
-        //         // You can unslick at a given breakpoint now by adding:
-        //         // settings: "unslick"
-        //         // instead of a settings object
-        //     ]
-        // });
+        $('#work-for-etaps').slick({
+            dots: true,
+            infinite: false,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            // responsive: [
+            //     {
+            //         breakpoint: 1024,
+            //         settings: {
+            //             slidesToShow: 3,
+            //             slidesToScroll: 1,
+            //             infinite: true,
+            //             dots: true
+            //         }
+            //     }
+            //     // You can unslick at a given breakpoint now by adding:
+            //     // settings: "unslick"
+            //     // instead of a settings object
+            // ]
+        });
 
     }
 }).resize()
